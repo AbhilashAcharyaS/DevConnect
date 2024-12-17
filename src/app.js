03d64edf2ed3394f25abd5ec0ac2d66fe93d9d59
AbhilashAcharyaS <abhilashacharya599@gmail.com>
2024-12-17 11:46:40 +0530

    wildcard error handling

--- src/app.js
+++ src/app.js
@@ -3,13 +3,25 @@ const app = express();
 
 const {adminAuth} = require("./middlewares/auth");
 
-app.use("/admin", adminAuth)
+// app.use("/admin", adminAuth)
 
 app.get("/admin/data",(req,res, next)=>{
-    res.send("Admin data sent");
-    // console.log("user called but sent no response");
+
+    // try {
+        throw new Error("Abhjsjd");
+        // res.send("Admin data sent");
+        // console.log("user called but sent no response");        
+    // } catch (error) {
+    //     console.log("Error occured!");
+    //     res.status(500).send("Something went wrong by catch block!");        
+    // }
 });
 
+app.use("/", (err,req,res,next)=>{
+    if(err){
+        res.status(500).send("Something went wrong!");
+    }
+})
 
 
 app.listen(3000, ()=>console.log("Server Started"))
